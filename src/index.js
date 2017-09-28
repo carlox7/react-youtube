705dfbe29492177b252ffbd62258efe5f6c28653
Carlo Santos <carlox7@gmail.com>
2017-09-28 16:05:01 -0700

    simple youtube app made with react

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import _ from 'lodash';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+
+    this.videoSearch('corgi puppies');
+  }
+
+  videoSearch(term){
+    YTSearch({key: __API_KEY__, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0],
+      });
+    });
+  }
+
+  render(){
+    const videoSearch = _.debounce((term) => this.videoSearch(term), 300);
+    return(
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
